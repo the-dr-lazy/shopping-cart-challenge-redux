deba42f0dd6bfcca7a0373416644fcd9d933d79e
Mohammad Hasani <thebrodmann@protonmail.com>
2020-03-28 19:10:21 +0430

    feat: add cart row component

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { singularNounToPluralByQuantity } from '~/utils'
+import * as Store from '~/store'
 
 type MiniProps = {
   cartQuantitySum: number
@@ -14,4 +15,73 @@ export function mini({ cartQuantitySum }: MiniProps) {
       : `Your cart is empty.`
 
   return <div>{message}</div>
+}
+
+type RowProps = {
+  product: Store.Product
+  quantity: number
+  onAddToCart?: (id: Store.ProductId) => void
+  onRemoveFromCart?: (id: Store.ProductId) => void
+  onAbsoluteRemoveFromCart?: (id: Store.ProductId) => void
+}
+
+export function row({
+  product,
+  quantity,
+  onAddToCart,
+  onRemoveFromCart,
+  onAbsoluteRemoveFromCart,
+}: RowProps) {
+  function handleIncrementQuantityButtonClick() {
+    onAddToCart!(product.id)
+  }
+
+  function handleDecrementQuantityButtonClick() {
+    onRemoveFromCart!(product.id)
+  }
+
+  function handleRemoveProductFromCartButtonClick() {
+    onAbsoluteRemoveFromCart!(product.id)
+  }
+
+  const incrementQuantityButton = onAddToCart && (
+    <button
+      aria-label="Increase quantity"
+      onClick={handleIncrementQuantityButtonClick}
+    >
+      +1
+    </button>
+  )
+  const decrementQuantityButton = onRemoveFromCart && quantity > 1 && (
+    <button
+      aria-label="Decrease quantity"
+      onClick={handleDecrementQuantityButtonClick}
+    >
+      -1
+    </button>
+  )
+  const removeProductFromCartButton = onAbsoluteRemoveFromCart && (
+    <button
+      aria-label="Remove product from cart"
+      onClick={handleRemoveProductFromCartButtonClick}
+    >
+      Remove 🗑
+    </button>
+  )
+
+  return (
+    <tr key={product.id}>
+      <td>{product.name}</td>
+      <td>{quantity}</td>
+      <td>${product.price}</td>
+      <td>${quantity * product.price}</td>
+      <td>
+        {[
+          incrementQuantityButton,
+          decrementQuantityButton,
+          removeProductFromCartButton,
+        ]}
+      </td>
+    </tr>
+  )
 }
--- test/components/cart.spec.tsx
+++ test/components/cart.spec.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 
 import * as Cart from '~/components/cart'
+
+import * as Data from '../data'
 
 describe('<Cart.mini />', () => {
   describe('when the sum of the cart quantity is zero', () => {
@@ -25,6 +27,172 @@ describe('<Cart.mini />', () => {
       const { container } = render(<Cart.mini cartQuantitySum={11} />)
 
       expect(container.firstChild).toHaveTextContent(/11/)
+    })
+  })
+})
+
+describe('<Cart.row />', () => {
+  function renderIntoTBody(reactElement: React.ReactElement) {
+    const tBody = document.createElement('tbody')
+
+    return render(reactElement, { container: document.body.appendChild(tBody) })
+  }
+
+  it('should render product name', () => {
+    const { queryByText } = renderIntoTBody(
+      <Cart.row product={Data.Product.a} quantity={2} />
+    )
+
+    const element = queryByText(Data.Product.a.name, { exact: false })
+
+    expect(element).not.toBeNull()
+  })
+
+  it('should render product quantity', () => {
+    const { queryByText } = renderIntoTBody(
+      <Cart.row product={Data.Product.a} quantity={3} />
+    )
+
+    expect(queryByText('3')).not.toBeNull()
+  })
+
+  it('should render product price', () => {
+    const { queryByText } = renderIntoTBody(
+      <Cart.row product={Data.Product.a} quantity={3} />
+    )
+
+    const element = queryByText(`$${Data.Product.a.price}`, { exact: false })
+
+    expect(element).not.toBeNull()
+  })
+
+  it('should render total price', () => {
+    const { queryByText } = renderIntoTBody(
+      <Cart.row product={Data.Product.b} quantity={5} />
+    )
+
+    const element = queryByText(`$${Data.Product.b.price * 5}`, {
+      exact: false,
+    })
+
+    expect(element).not.toBeNull()
+  })
+
+  describe('when quantity is one', () => {
+    it('should not render remove from the cart button', () => {
+      const onRemoveFromCart = jest.fn()
+
+      const { queryByLabelText } = renderIntoTBody(
+        <Cart.row
+          product={Data.Product.a}
+          quantity={1}
+          onRemoveFromCart={onRemoveFromCart}
+        />
+      )
+
+      expect(queryByLabelText(/decrease quantity/i)).toBeNull()
+    })
+  })
+
+  describe('when click on add to the cart button', () => {
+    it('should call onAddToCart callback', () => {
+      const onAddToCart = jest.fn()
+
+      const { getByLabelText } = renderIntoTBody(
+        <Cart.row
+          product={Data.Product.a}
+          quantity={1}
+          onAddToCart={onAddToCart}
+        />
+      )
+
+      fireEvent.click(getByLabelText(/increase quantity/i))
+
+      expect(onAddToCart).toBeCalled()
+    })
+
+    it('should apply correct product ID to onAddToCart callback', () => {
+      const onAddToCart = jest.fn()
+
+      const { getByLabelText } = renderIntoTBody(
+        <Cart.row
+          product={Data.Product.b}
+          quantity={1}
+          onAddToCart={onAddToCart}
+        />
+      )
+
+      fireEvent.click(getByLabelText(/increase quantity/i))
+
+      expect(onAddToCart).toBeCalledWith(Data.Product.b.id)
+    })
+  })
+
+  describe('when click on remove from the cart button', () => {
+    it('should call onRemoveFromCart callback', () => {
+      const onRemoveFromCart = jest.fn()
+
+      const { getByLabelText } = renderIntoTBody(
+        <Cart.row
+          product={Data.Product.a}
+          quantity={2}
+          onRemoveFromCart={onRemoveFromCart}
+        />
+      )
+
+      fireEvent.click(getByLabelText(/decrease quantity/i))
+
+      expect(onRemoveFromCart).toBeCalled()
+    })
+
+    it('should apply correct product ID to onRemoveFromCart callback', () => {
+      const onRemoveFromCart = jest.fn()
+
+      const { getByLabelText } = renderIntoTBody(
+        <Cart.row
+          product={Data.Product.b}
+          quantity={2}
+          onRemoveFromCart={onRemoveFromCart}
+        />
+      )
+
+      fireEvent.click(getByLabelText(/decrease quantity/i))
+
+      expect(onRemoveFromCart).toBeCalledWith(Data.Product.b.id)
+    })
+  })
+
+  describe('when click on absolute remove from the cart button', () => {
+    it('should call onAbsoluteRemoveFromCart callback', () => {
+      const onAbsoluteRemoveFromCart = jest.fn()
+
+      const { getByLabelText } = renderIntoTBody(
+        <Cart.row
+          product={Data.Product.a}
+          quantity={1}
+          onAbsoluteRemoveFromCart={onAbsoluteRemoveFromCart}
+        />
+      )
+
+      fireEvent.click(getByLabelText(/remove product/i))
+
+      expect(onAbsoluteRemoveFromCart).toBeCalled()
+    })
+
+    it('should apply correct product ID to onAbsoluteRemoveFromCart callback', () => {
+      const onAbsoluteRemoveFromCart = jest.fn()
+
+      const { getByLabelText } = renderIntoTBody(
+        <Cart.row
+          product={Data.Product.b}
+          quantity={1}
+          onAbsoluteRemoveFromCart={onAbsoluteRemoveFromCart}
+        />
+      )
+
+      fireEvent.click(getByLabelText(/remove product/i))
+
+      expect(onAbsoluteRemoveFromCart).toBeCalledWith(Data.Product.b.id)
     })
   })
 })
