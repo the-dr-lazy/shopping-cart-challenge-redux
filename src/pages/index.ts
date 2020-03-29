961f7d8dbb765d4ff9976e76b07d3bad9d8f39fa
Mohammad Hasani <thebrodmann@protonmail.com>
2020-03-29 14:24:54 +0430

    feat: export pages in public API

---
+++ src/pages/index.ts
@@ -0,0 +1,9 @@
+// Although we are using TypeScript >= 3.8 due to an issue
+// in ts-loader#1070 we are not able to transpile it through webpack.
+// For more information see
+// https://github.com/TypeStrong/ts-loader/issues/1070
+
+import * as Home from './home'
+import * as Cart from './cart'
+
+export { Home, Cart }
